d1239a1ee891a28a5ff021d39780e320d9a8af26
Igor Lozovyk <i.lozovyk@gmail.com>
2019-10-16 15:52:43 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const { watch, src, series, dest, parallel } = require('gulp');
+const pug = require('gulp-pug');
+const sass = require ('gulp-sass');
+
+
+//TODO: babel
+//TODO: uglify
+//TODO: clean-css
+//TODO: webpack
+//TODO: fancy-log
+//TODO: node-sass
+//TODO: rename
+//TODO: Sourcemap
+//TODO: browser-sync
+//TODO: iconfont, css
