943366f28e39ab0a5efda115f075490c8b8f2c0c
Igor Lozovyk <i.lozovyk@gmail.com>
2019-10-16 16:10:22 +0300

    add gulp consts

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
 const { watch, src, series, dest, parallel } = require('gulp');
 const pug = require('gulp-pug');
-const sass = require ('gulp-sass');
+const sass = require('gulp-sass');
+const logs = require('fancy-log');
+const babel = require('gulp-babel');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const iconfont = require('gulp-iconfont');
+const cleanCss = require('gulp-clean-css');
+const sourcemap = require('gulp-sourcemap');
+const iconfontCSS = require('gulp-iconfont-css');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
+sass.compiler = require('node-sass');
+
+const fontName = 'iconFont';
 
 //TODO: babel
 //TODO: uglify
@@ -10,6 +24,8 @@ const sass = require ('gulp-sass');
 //TODO: fancy-log
 //TODO: node-sass
 //TODO: rename
+//TODO: concat
+//TODO: autoprefixer
 //TODO: Sourcemap
 //TODO: browser-sync
 //TODO: iconfont, css
